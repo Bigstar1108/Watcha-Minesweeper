bb7c34de85f96429a54968560b40933bed5a6fd7
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 13:59:21 +0900

    [Fix]
    connect store and dispatch
    create some func
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- minesweeper/src/containers/gameHome.js
+++ minesweeper/src/containers/gameHome.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as actions from '../modules/actions';
+import { setRandomMines } from '../lib';
+import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import '../App.css';
+
 import GameHeader from '../components/gameHome/game-header';
+import GameArea from '../components/gameHome/game-area';
+import GamePopup from '../components/gameHome/game-popup';
 
 const GameHome = () => {
+    const { time, mines, popupText } = useSelector((state) => state.Minesweeper);
+    const dispatch = useDispatch();
+
+    const handleCreateMines = () => {
+        const boxObj = setRandomMines();
+        dispatch(actions.createMine(boxObj));
+    };
+
+    const handleRestartGame = () => dispatch(actions.restartGame());
+    
+    useEffect(() => {
+        handleCreateMines();
+    }, [])
+
     return(
         <div>
             <GameHeader
-            mines = {10}
-            time = {9}
+                time = {time}
+                mines = {mines}
+                handleRestartGame = {handleRestartGame}
+            />
+            <GameArea />
+            <GamePopup
+                time = {time}
+                popupText = {popupText}
+                handleCreateMines = {handleCreateMines}
             />
         </div>
     );
+};
+
+GameHome.propTypes = {
+    time: PropTypes.number,
+    mines: PropTypes.number,
+    popupText: PropTypes.string,
+    handleCreateMines: PropTypes.func,
+    handleRestartGame: PropTypes.func,
+};
+
+GameHome.defaultProps = {
+    time: 0,
+    mines: 9,
+    popupText: '',
+    handleCreateMines: () => console.warn('handleCreateMines not defined'),
+    handleRestartGame: () => console.warn('handleRestartGame not defined')
 };
 
 export default GameHome;
