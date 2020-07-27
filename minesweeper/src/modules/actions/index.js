2afc6a1bcf60857809b6f0147d840ede7000be85
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 23:31:07 +0900

    [Fix]
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- minesweeper/src/modules/actions/index.js
+++ minesweeper/src/modules/actions/index.js
@@ -11,6 +11,7 @@ export const GAME_OVER = 'GAME_OVER';
 export const FINISH_GAME = 'FINISH_GAME';
 export const START_TIME = 'START_TIME';
 
+// 지뢰찾기 맵 생성
 export function createMine(obj){
     return{
         type: CREATE_MINE,
@@ -18,6 +19,7 @@ export function createMine(obj){
     };
 };
 
+// 사용자 클릭 후 변경된 맵 적용
 export function updateBox(obj, num){
     return{
         type: UPDATE_BOX,
@@ -26,12 +28,15 @@ export function updateBox(obj, num){
     };
 };
 
+// 게임 다시 시작
 export function restartGame(){
     return{
         type: RESTART_GAME,
     };
 };
 
+
+// 오른쪽 마우스 클릭시 깃발 생성
 export function createFlag(id){
     return{
         type: CREATE_FLAG,
@@ -39,6 +44,7 @@ export function createFlag(id){
     };
 };
 
+// 오른쪽 마우스 클릭시 깃발 삭제
 export function deleteFlag(id){
     return{
         type: DELETE_FLAG,
@@ -46,6 +52,7 @@ export function deleteFlag(id){
     };
 };
 
+// 주변에 폭탄이 있는 수를 담은 Box를 클릭했을 때
 export function clickNumber(id, num){
     return{
         type: CLICK_NUMBER,
@@ -54,6 +61,7 @@ export function clickNumber(id, num){
     };
 };
 
+// 아무 값도 들어있지 않은 빈 Box를 클릭했을 때
 export function clickEmpty(id){
     return{
         type: CLICK_EMPTY,
@@ -61,6 +69,7 @@ export function clickEmpty(id){
     };
 };
 
+// 지뢰를 밟아 게임이 끝났을 때
 export function gameOver(id){
     return{
         type: GAME_OVER,
@@ -68,12 +77,14 @@ export function gameOver(id){
     };
 };
 
+// 지뢰를 모두 찾아 게임이 끝났을 때
 export function finishGame(){
     return{
         type: FINISH_GAME,
     };
 };
 
+// 게임 시작 시 Timer값을 올려주는 역할
 export function startTime(){
     return{
         type: START_TIME,
