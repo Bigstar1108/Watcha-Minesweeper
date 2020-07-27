dbc6ed07004612ea2f647c47a7cff4f9d2177cdc
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 20:14:33 +0900

    [Fix]
    add modal func
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- minesweeper/src/components/gameHome/game-popup.js
+++ minesweeper/src/components/gameHome/game-popup.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import AwardModal from '../../components/gameHome/awardModal';
 
 const GamePopup = ({ popupText, handleCreateMines }) => {
+    const [Modal, setModal] = useState(false);
+
+    const handleonClick = (type) => {
+        switch(type){
+            case '지뢰찾기를 다시 시작하시겠습니까?':
+                setModal(false);
+                handleCreateMines();
+                return null;
+            case '지뢰 발견! 다시 시작하기':
+                setModal(false);
+                handleCreateMines();
+                return null;
+            case '지뢰 찾기 클리어! 랭킹에 등록해보세요!':
+                setModal(false);
+                handleConfirm();
+                return null;
+            default:
+                return null;
+        }
+    }
+
+    const handleConfirm = () => {
+        if(window.confirm('랭킹에 기록하시겠습니까?')){
+            setModal(true);
+        }else{
+            handleCreateMines();
+        }
+    };
+
     // popUp Text의 유무에 따라 다르게 출력
     let popUpContainer = null;
     if(popupText !== ''){
         popUpContainer = 
-        <div className = "popup-container">
-            <span onClick = {() => handleCreateMines()}>
-                {popupText}
-            </span>
-        </div>
+        <>
+            <div className = "popup-container">
+                <span onClick = {() => handleonClick(popupText)}>
+                    {popupText}
+                </span>
+            </div>
+            {
+                Modal ? <AwardModal /> : null
+            }
+        </>
     }
     return(
         <div>
@@ -20,11 +55,13 @@ const GamePopup = ({ popupText, handleCreateMines }) => {
 };
 
 GamePopup.propTypes = {
+    Modal: PropTypes.bool,
     popupText: PropTypes.string,
     handleCreateMines: PropTypes.func,
 };
 
 GamePopup.defaultProps = {
+    Modal: false,
     popupText: '',
     handleCreateMines: () => console.warn('handleCreateMines not defined'),
 };
