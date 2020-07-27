457d8ec43984c6cf4b89da370ddfe0c36aef0cec
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 21:47:54 +0900

    Updates

---
+++ precache-manifest.5ef8c94b5766880593c728fe7dff95d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ace3711bd9f099e5c5d0d191ab357e",
+    "url": "/Watcha-Minesweeper/index.html"
+  },
+  {
+    "revision": "3e67f6e68e03d40055ee",
+    "url": "/Watcha-Minesweeper/static/css/main.f4493637.chunk.css"
+  },
+  {
+    "revision": "6a42bf23d3c7d06782ed",
+    "url": "/Watcha-Minesweeper/static/js/2.3b01b715.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/Watcha-Minesweeper/static/js/2.3b01b715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e67f6e68e03d40055ee",
+    "url": "/Watcha-Minesweeper/static/js/main.e9cd479c.chunk.js"
+  },
+  {
+    "revision": "e0da99cff06cb41d1f99",
+    "url": "/Watcha-Minesweeper/static/js/runtime-main.be55ee68.js"
+  }
+]);
