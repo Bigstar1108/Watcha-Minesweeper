88bb65fbbd33ea916f1df92f25888f56fc461e0b
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 21:36:32 +0900

    [Add]
    add footer
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Main from './containers/Main';
 import GameHome from './containers/gameHome';
+import Footer from './components/Footer/Footer';
 import './App.scss';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <Route exact path = '/' component = {Main} />
           <Route exact path = '/minesweeper' component = {GameHome} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </div>
   );
