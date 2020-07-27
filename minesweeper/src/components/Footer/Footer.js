b2efad475604f26f7276eebea2eedfe6fa605649
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 21:37:47 +0900

    [Add]
    add footer
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

---
+++ minesweeper/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <div className = "Footer">
+      <a href = "https://github.com/Bigstar1108/Watcha-Minesweeper">Made By Bigstar1108(Github)</a>
+    </div>
+  );
+}
+
+export default Footer;
