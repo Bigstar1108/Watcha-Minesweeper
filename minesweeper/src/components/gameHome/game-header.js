0c32f8a1ae1c7f06a761729d9eb4530bce2e65e8
Bigstar1108 <daesung09854@gmail.com>
2020-07-27 15:24:24 +0900

    [Fix]
    add button and fix proptypes
    
    Co-Authored-By: Daeseong Hwang <65744080+bigstar1108@users.noreply.github.com>

--- minesweeper/src/components/gameHome/game-header.js
+++ minesweeper/src/components/gameHome/game-header.js
@@ -6,16 +6,19 @@ const GameHeader = ({time, mines, handleRestartGame}) => {
         <div>
             <p>남은 지뢰 : {mines}</p>
             <p>Time : {time}</p>
+            <button type = "button" onClick = {() => handleRestartGame()}>다시 시작하기</button>
         </div>
     );
 };
 
 GameHeader.propTypes = {
-    mines: PropTypes.number
+    mines: PropTypes.number,
+    handleRestartGame: PropTypes.func,
 };
 
 GameHeader.defaultProps = {
     mines: 10,
+    handleRestartGame: () => console.warn('handleRestartGame not defined'),
 };
 
 export default GameHeader;
